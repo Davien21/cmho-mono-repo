5159b1621ff9f48fbf8e910708a08687f312fbdf
davien21 <chidiebereekennia@gmail.com>
2025-12-01 23:01:53 +0100

    fix: Redirect to login when logged out on root page
    chore: Remove linting GitHub workflow

--- apps/frontend/src/AppRoutes.tsx
+++ apps/frontend/src/AppRoutes.tsx
@@ -1,21 +1,29 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
-import NotFoundPage from "@/pages/modules/salary-manager/NotFoundPage";
-import EmployeesPage from "@/pages/modules/salary-manager/EmployeesPage";
-import DashboardPage from "@/pages/modules/salary-manager/DashboardPage";
-import LoginPage from "@/pages/LoginPage";
-import PaymentHistoryPage from "@/pages/modules/salary-manager/PaymentHistoryPage";
-import TransferDetailsPage from "@/pages/modules/salary-manager/TransferDetailsPage";
-import AppSelectionPage from "@/pages/AppSelectionPage";
-import InventoryPage from "@/pages/modules/inventory-manager/InventoryPage";
-import InventorySettingsPage from "@/pages/modules/inventory-manager/InventorySettingsPage";
-import StockEntriesPage from "@/pages/modules/inventory-manager/StockEntriesPage";
-import StockChangesPage from "@/pages/modules/inventory-manager/StockChangesPage";
+import NotFoundPage from '@/pages/modules/salary-manager/NotFoundPage';
+import EmployeesPage from '@/pages/modules/salary-manager/EmployeesPage';
+import DashboardPage from '@/pages/modules/salary-manager/DashboardPage';
+import LoginPage from '@/pages/LoginPage';
+import PaymentHistoryPage from '@/pages/modules/salary-manager/PaymentHistoryPage';
+import TransferDetailsPage from '@/pages/modules/salary-manager/TransferDetailsPage';
+import AppSelectionPage from '@/pages/AppSelectionPage';
+import InventoryPage from '@/pages/modules/inventory-manager/InventoryPage';
+import InventorySettingsPage from '@/pages/modules/inventory-manager/InventorySettingsPage';
+import StockEntriesPage from '@/pages/modules/inventory-manager/StockEntriesPage';
+import StockChangesPage from '@/pages/modules/inventory-manager/StockChangesPage';
+import ProtectedRoute from '@/components/ProtectedRoute';
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<AppSelectionPage />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <AppSelectionPage />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/salary" element={<DashboardPage />} />
       <Route path="/salary/employees" element={<EmployeesPage />} />
